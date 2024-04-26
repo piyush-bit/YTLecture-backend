f8999370e079d5c975d7b951137cc6e53367f631
Piyush Kumar <mr.piyush.cv@gmail.com>
2024-04-27 01:09:58 +0530

    set secure true attribute in set cookie

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -62,6 +62,7 @@ export const signin = async (req, res, next) => {
       .cookie("access_token", token, {
         httpOnly: true,
         sameSite: "none",
+        secure : true
       })
       .status(200)
       .json(others);
