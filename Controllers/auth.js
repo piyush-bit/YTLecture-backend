122c4a8b7c6b27a7b2877e46f76801d06fe43652
Piyush Kumar <mr.piyush.cv@gmail.com>
2024-06-09 19:14:26 +0530

    Sign Out fix

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -77,7 +77,7 @@ export const signout = async (req, res, next) => {
     res.clearCookie("access_token", {
       httpOnly: true,
       // domain: process.env.CORS_ADD
-      domain: 'localhost , netlify.app',
+      domain: process.env.DOMAIN,
     });
     res
       .status(200).json({message : "done"});
