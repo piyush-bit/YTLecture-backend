7cc07fd137f1629d58745d0a42770cc2ff61ea89
piyush-bit <mr.piyush.cv@gmail.com>
2025-05-17 15:05:04 +0530

    remove auth for creating

--- Routes/createCourse.js
+++ Routes/createCourse.js
@@ -3,7 +3,7 @@ const router = express.Router();
 import {create} from '../Controllers/Course.js'
 import { verifyToken } from "../verifyTocken.js"
 import { createLanguage, createTag } from "../Controllers/search.js";
-router.post('/',verifyToken,create)
+router.post('/',create)
 router.post('/createtag',createTag)
 router.post('/createlanguage',createLanguage)
 export default router;
